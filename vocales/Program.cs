b6b9972a2e0a260348205aac5c211a97aa3cab3c
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-05-11 19:27:43 -0600

    Agregar archivos de proyecto.

---
+++ vocales/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+string texto;
+int contador;
+contador = 0;
+Console.WriteLine();
+Console.WriteLine("Vocales en el texto");
+Console.WriteLine();
+Console.Write("Ingrese el texto: ");
+texto = Console.ReadLine().ToLower();
+Console.WriteLine();
+
+foreach (char vocales in texto)
+{
+    if (vocales == 'a' || vocales == 'e' || vocales == 'i' || vocales == 'o' || vocales == 'u' || vocales == 'á' || vocales == 'é' || vocales == 'í' || vocales == 'ó' || vocales == 'ú')
+    {
+        contador++;
+    }
+}
+Console.WriteLine("El texto tiene " + contador + " vocales");
+
